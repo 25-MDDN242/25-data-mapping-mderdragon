fba432329dd5f5d39c62c67bb9ed2ae09ebd6b9d
mderdragon <emma.r.nash@gmail.com>
2025-06-04 18:57:23 +1200

    vines texture

--- sketch.js
+++ sketch.js
@@ -1,51 +1,72 @@
 let sourceImg=null;
 let maskImg=null;
-let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
-  //console.log(p5.Renderer2D);
-
+  textImg = loadImage("texture.png");
 }
+
 function setup () {
   let main_canvas = createCanvas(1920, 1080);
   main_canvas.parent('canvasContainer');
 
   imageMode(CENTER);
   noStroke();
-  background(255, 0, 0);
+  background(0, 0, 128);
   sourceImg.loadPixels();
   maskImg.loadPixels();
+  textImg.loadPixels();
 }
 
+let X_STOP = 1920;
+let Y_STOP = 1080;
+
+let renderCounter=0;
 function draw () {
-  for(let i=0;i<4000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pixData = sourceImg.get(x, y);
-    let maskData = maskImg.get(x, y);
-    fill(pixData);
-    if(maskData[0] > 128) {
-      let pointSize = 10;
-      ellipse(x, y, pointSize, pointSize);
-    }
-    else {
-      let pointSize = 20;
-      rect(x, y, pointSize, pointSize);    
+  let num_lines_to_draw = 40;
+  // get one scanline
+  for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<1080; j++) {
+    for(let i=0; i<X_STOP; i++) {
+      colorMode(RGB);
+      let pix = sourceImg.get(i, j);
+      // create a color from the values (always RGB)
+      let col = color(pix);
+      let mask = maskImg.get(i, j);
+      let tex = textImg.get(i, j);
+
+      if(mask[0] < 128) {
+        set(i, j, pix);
+      }
+      else {
+        let new_col = [0, 0, 0, 255];
+        // for(let k=0; k<2; k++) {
+        //   new_col[k] = map(40, 0, 100, pix[k], tex[k]);
+        // }
+
+        new_col[0] = map(70, 0, 100, pix[0], tex[0]);
+        new_col[1] = map(60, 0, 100, pix[1], tex[1]);
+        new_col[2] = map(80, 0, 100, pix[2], tex[2]);
+
+        // let new_col = color(h, s,  newBrt);
+        set(i, j, new_col);
+      }
     }
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  renderCounter = renderCounter + num_lines_to_draw;
+  updatePixels();
+
+  // print(renderCounter);
+  if(renderCounter > Y_STOP) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
-    // saveArtworkImage(outputFile);
+  //saveArtworkImage(outputFile);
   }
 }
 
